5e57c10821c88183eeb7f96494588fe90eb57b87
rcdev99 <ricardo06.rodrigues@gmail.com>
2021-05-03 14:48:36 -0300

    feat: Implementado método para exclusao de cursos.

--- view/cursos/listar-cursos.php
+++ view/cursos/listar-cursos.php
@@ -9,7 +9,7 @@
         <?php foreach ($cursos as $curso): ?>
             <li class="list-group-item d-flex justify-content-between">
                 <?= $curso->getDescricao(); ?>
-                <a href="/excluir-curso" class="btn btn-danger btn-sm">
+                <a href="/excluir-curso?id=<?= $curso->getId(); ?>" class="btn btn-danger btn-sm">
                     Excluir
                 </a>
             </li>
